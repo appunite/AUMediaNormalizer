389f57defd6a10b904ef72950865212f3a48c801
Krzysztof <krzysztof.kapitan@gmail.com>
2015-07-07 14:42:58 +0200

    add tests for convienience methods

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -31,7 +31,7 @@
     [super tearDown];
 }
 
-- (void)testProcessingImage{
+- (void)testProcessingImageWithThumbnailBlock{
     AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
     XCTAssertNotNil(mediaProcessing.bucket);
     
@@ -43,7 +43,7 @@
     [info setObject:@"public.image" forKey:UIImagePickerControllerMediaType];
     [info setObject:exampleImage forKey:UIImagePickerControllerOriginalImage];
     
-    [mediaProcessing processImageWithPickerParams:info completitionBlock:^(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL) {
+    [mediaProcessing processImageWithPickerParams:info thumbnailBlock:^(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL) {
         XCTAssertNotNil(fileURL);
         XCTAssertNotNil(thumbnailURL);
         
@@ -64,7 +64,41 @@
     }];
 }
 
-- (void)testProcessingVideo{
+- (void)testProcessingImageWithAttachmentBlock{
+    AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
+    XCTAssertNotNil(mediaProcessing.bucket);
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"ImageExpectation"];
+    
+    UIImage *exampleImage = [UIImage imageNamed:@"example_image.jpg"];
+    
+    NSMutableDictionary *info = [NSMutableDictionary new];
+    [info setObject:@"public.image" forKey:UIImagePickerControllerMediaType];
+    [info setObject:exampleImage forKey:UIImagePickerControllerOriginalImage];
+    [mediaProcessing processImageWithPickerParams:info attachmentBlock:^(AUAttachmentFile *attachmentFile) {
+    
+        XCTAssertNotNil(attachmentFile.coverURL);
+        XCTAssertNotNil(attachmentFile.sourceURL);
+        
+        UIImage* fullImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:attachmentFile.sourceURL]];
+        UIImage* thumbnailImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:attachmentFile.coverURL]];
+        
+        XCTAssertNotNil(fullImage);
+        XCTAssertNotNil(thumbnailImage);
+        
+        XCTAssertEqual([attachmentFile.attributes[@"width"] doubleValue], fullImage.size.width);
+        XCTAssertEqual([attachmentFile.attributes[@"height"] doubleValue], fullImage.size.height);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        NSLog(@"%@",error);
+    }];
+}
+
+
+- (void)testProcessingVideoWithDefaultSettingsAndThumbnailBlock{
     
     AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
     XCTAssertNotNil(mediaProcessing.bucket);
@@ -92,6 +126,104 @@
         NSLog(@"%@",error);
     }];
 }
+
+- (void)testProcessingVideoWithCustomSettingsAndThumbnailBlock{
+    
+    AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
+    XCTAssertNotNil(mediaProcessing.bucket);
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"VideoExpectation"];
+    XCTestExpectation *settingsExpectation = [self expectationWithDescription:@"SettingsExpectation"];
+    
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"example_movie" ofType:@".mov"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    NSMutableDictionary *info = [NSMutableDictionary new];
+    [info setObject:@"public.movie" forKey:UIImagePickerControllerMediaType];
+    [info setObject:url forKey:UIImagePickerControllerMediaURL];
+    [mediaProcessing processVideoWithPickerParams:info thumbnailBlock:^(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL) {
+        
+        XCTAssertNotNil(fileURL);
+        XCTAssertNotNil(thumbnailURL);
+        UIImage* thumbnailImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:thumbnailURL]];
+        XCTAssertNotNil(thumbnailImage);
+
+    } encoderSettingsBlock:^(SDAVAssetExportSession *encoder) {
+        XCTAssertNotNil(encoder);
+        [settingsExpectation fulfill];
+        
+    } completitionBlock:^(NSUUID *process, AVAssetExportSessionStatus status, NSError *error) {
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        NSLog(@"%@",error);
+    }];
+}
+
+- (void)testProcessingVideoWithCustomSettingsAndAttachmentBlock{
+    
+    AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
+    XCTAssertNotNil(mediaProcessing.bucket);
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"VideoExpectation"];
+    XCTestExpectation *settingsExpectation = [self expectationWithDescription:@"SettingsExpectation"];
+    
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"example_movie" ofType:@".mov"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    NSMutableDictionary *info = [NSMutableDictionary new];
+    [info setObject:@"public.movie" forKey:UIImagePickerControllerMediaType];
+    [info setObject:url forKey:UIImagePickerControllerMediaURL];
+    [mediaProcessing processVideoWithPickerParams:info attachmentBlock:^(AUAttachmentFile *attachmentFile) {
+    
+        XCTAssertNotNil(attachmentFile.sourceURL);
+        XCTAssertNotNil(attachmentFile.coverURL);
+        UIImage* thumbnailImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:attachmentFile.coverURL]];
+        XCTAssertNotNil(thumbnailImage);
+        
+    } encoderSettingsBlock:^(SDAVAssetExportSession *encoder) {
+        XCTAssertNotNil(encoder);
+        [settingsExpectation fulfill];
+        
+    } completitionBlock:^(NSUUID *process, AVAssetExportSessionStatus status, NSError *error) {
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        NSLog(@"%@",error);
+    }];
+}
+
+- (void)testProcessingVideoWithDefaultSettingsAndAttachmentBlock{
+    
+    AUMediaProcessing* mediaProcessing = [[AUMediaProcessing alloc] init];
+    XCTAssertNotNil(mediaProcessing.bucket);
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"VideoExpectation"];
+    
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"example_movie" ofType:@".mov"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    NSMutableDictionary *info = [NSMutableDictionary new];
+    [info setObject:@"public.movie" forKey:UIImagePickerControllerMediaType];
+    [info setObject:url forKey:UIImagePickerControllerMediaURL];
+    [mediaProcessing processVideoWithPickerParams:info attachmentBlock:^(AUAttachmentFile *attachmentFile) {
+        
+        XCTAssertNotNil(attachmentFile.sourceURL);
+        XCTAssertNotNil(attachmentFile.coverURL);
+        UIImage* thumbnailImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:attachmentFile.coverURL]];
+        XCTAssertNotNil(thumbnailImage);
+        
+    } completitionBlock:^(NSUUID *process, AVAssetExportSessionStatus status, NSError *error) {
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        NSLog(@"%@",error);
+    }];
+}
+
 
 @end
 
