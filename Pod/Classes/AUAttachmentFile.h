29b13c53156287e1cdfc5dd62c880a97cc78511f
Krzysztof <krzysztof.kapitan@gmail.com>
2015-07-07 16:44:38 +0200

    improved documentation

--- Pod/Classes/AUAttachmentFile.h
+++ Pod/Classes/AUAttachmentFile.h
@@ -9,26 +9,130 @@
 #import "Mantle.h"
 //Models
 
+/*!
+ 
+ * @class AUAttachmentFile
+ 
+ * @discussion This class is responsible for storing information about thumbnail image and the original content.
+
+ */
+
 @interface AUAttachmentFile : MTLModel
 
+/*!
+ 
+ * @property uid
+ 
+ * @abstract unique id of the attachment file.
+ 
+ */
+
 @property (nonatomic, strong, readonly) NSUUID *uid;
+
+/*!
+ 
+ * @property MIMEtype
+ 
+ * @abstract type of the attachment file (image/video).
+ 
+ */
+
 @property (nonatomic, strong, readonly) NSString *MIMEtype;
 
-// file size in bytes
+/*!
+ 
+ * @property fileSize
+ 
+ * @abstract file size in bytes.
+ 
+ */
+
 @property (nonatomic, assign, readonly) long long fileSize;
 
-// relative path to source file
+/*!
+ 
+ * @property sourcePath
+ 
+ * @abstract relative path to source file.
+ 
+ */
+
 @property (nonatomic, strong, readonly) NSString *sourcePath;
+
+/*! 
+ 
+ * Getter for URL to source file.
+ 
+ * @result
+ *     Returns URL to source file.
+ 
+*/
+
 - (NSURL *)sourceURL;
 
-// relative path to cover thumbnail image file
+/*!
+ 
+ * @property coverPath
+ 
+ * @abstract relative path to thumbnail file.
+ 
+ */
+
 @property (nonatomic, strong, readonly) NSString *coverPath;
+
+/*!
+ 
+ * Getter for URL to cover file.
+ 
+ * @result
+ *     Returns URL to cover file.
+ 
+ */
+
 - (NSURL *)coverURL;
 
-// describe attachment destination
+/*!
+ 
+ * @property attributes
+ 
+ * @abstract additional information about source file.
+ * @discussion 
+ *        Keys:
+ *          - width  - width of attachment.
+ *          - height - height of attachment.
+ 
+ */
+
+
 @property (nonatomic, strong, readonly) NSDictionary *attributes;
 
-// 
+/*!
+ 
+ * Creates new AUAttachmentFile.
+ 
+ * @param sourceURL
+ *     URL which points to the source file.
+ 
+ * @param mimetype
+ *     Type of media (image/video).
+ 
+ * @param fileSize
+ *     Size of the source file in bytes.
+ 
+ * @param coverURL
+ *     URL which points to the thumbnail file.
+ 
+ * @param attributes
+ *     Dictionary with additional information about source file.
+ 
+ * @param identifier
+ *     ID of the attachment file.
+ 
+ * @result 
+ *     Returns freshly created AUAttachmentFile.
+ 
+ */
+
 - (instancetype)initWithSourceURL:(NSURL *)sourceURL
                          MIMEType:(NSString *)mimetype
                          fileSize:(long long)fileSize
@@ -36,13 +140,53 @@
                        attributes:(NSDictionary *)attributes
                        identifier:(NSUUID *)identifier;
 
-// create attachment with image/jpeg MIME type and save image dimensions in attributes
+/*!
+ 
+ * Creates new AUAttachmentFile with image/jpeg MIME type and saves image dimensions in attributes dictionary.
+ 
+ * @param sourceURL
+ *     URL which points to the source file.
+ 
+ * @param coverURL
+ *     URL which points to the thumbnail file.
+ 
+ * @param size
+ *     Size (dimentions) of the source file.
+ 
+ * @param identifier
+ *     ID of the attachment file.
+ 
+ * @result
+ *     Returns freshly created AUAttachmentFile.
+ 
+ */
+
 - (instancetype)initWithImageSourceURL:(NSURL *)sourceURL
                               coverURL:(NSURL *)coverURL
                                   size:(CGSize)size
                             identifier:(NSUUID *)identifier;
 
-// create attachment with image/mp4 MIME type and save video dimensions in attributes
+/*!
+ 
+ * Creates new AUAttachmentFile with image/mp4 MIME type and saves video dimensions in attributes dictionary.
+ 
+ * @param sourceURL
+ *     URL which points to the source file.
+ 
+ * @param coverURL
+ *     URL which points to the thumbnail file.
+ 
+ * @param size
+ *     Size (dimentions) of the source file
+ 
+ * @param identifier
+ *     ID of the attachment file.
+ 
+ * @result
+ *     Returns freshly created AUAttachmentFile.
+ 
+ */
+
 - (instancetype)initWithVideoSourceURL:(NSURL *)sourceURL
                               coverURL:(NSURL *)coverURL
                                   size:(CGSize)size
--- Pod/Classes/AUMediaProcessing.h
+++ Pod/Classes/AUMediaProcessing.h
@@ -13,62 +13,233 @@
 #import <SDAVAssetExportSession/SDAVAssetExportSession.h>
 #import <AUMediaNormalizer/AUAttachmentFile.h>
 
+/*! 
+ 
+ * @class AUMediaProcessing
+ 
+ * @discussion This class is responsible for: 
+ *  - Creating thumbnail image based on the provided media.
+ *  - Storing both thumbnail and the original content in the path specified by bucketStoragePath.
+ *  - Exporting video media with audio and video settings specified by user.
+ 
+ */
+
 @interface AUMediaProcessing : NSObject
-// default generated thumbnail size
+
+/*!
+ 
+ * @property thumbnailSize
+ 
+ * @abstract The desired size of a thumbnail image (set by default).
+ 
+ */
+
 @property (nonatomic, assign) CGSize thumbnailSize;
 
-// file storage path
+/*!
+ 
+ * @property bucket
+ 
+ * @abstract File storage path.
+ 
+ */
+
 @property (nonatomic, strong, readonly) NSString *bucket;
 
-// Creates an instance of AUMediaProcessing class with file storage path specified by NSString.
+/*!
+ 
+ * Creates an instance of AUMediaProcessing class with file storage path specified by NSString.
+ 
+ * @param bucket
+ *     File storage path.
+ 
+ * @result
+ *     Returns freshly created instance of an AUMediaProcessing object.
+ 
+ */
+
 - (instancetype)initWithBucket:(NSString *)bucket;
 
-// Returns NSURL which points to the file storage directory.
+/*!
+    
+ * Getter for the file storage directory.
+ 
+ * @result
+ *     Returns NSURL which points to the file storage directory.
+*/
+
 + (NSURL *)bucketsStoragePath;
 
 /*!
- This method is responsible mainly for creating thumbnail image, based on the provided media, and storing both thumbnail and the original content in the path specified by bucketStoragePath.
  
- It takes as an input NSDictionary with similar structure to the UIImagePickerController's info dictionary.
- The basic key-values needed to be supplied are: 
- - UIImagePickerControllerMediaType -> type of the media being processed (image)
- - UIImagePickerControllerOriginalImage -> UIImage holding the original image
+ * Creates and stores thumbnail based on the original image.
  
- completionBlock is executed right after the end of processing image. The NSUUID variable holds the process unique id. The NSURL fileURL points to the original file, CGSize is the size of original image and NSURL thumbnailURL points to the created thumbnail.
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (image)
+ *      - UIImagePickerControllerOriginalImage -> UIImage holding the original image
+ 
+ * @param thumbnailBlock
+ *     This block is executed right after the end of processing image. 
+ *     Parameters:
+ *      - NSUUID process     - process unique id.
+ *      - NSURL fileURL      - points to the original file.
+ *      - CGSize size        - size of original image.
+ *      - NSURL thumbnailURL - points to the created thumbnail.
  
  */
 - (NSUUID *)processImageWithPickerParams:(NSDictionary *)params
                    thumbnailBlock:(void (^)(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL))block;
 
+/*!
+ 
+ * Creates and stores thumbnail based on the original image.
+ 
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (image)
+ *      - UIImagePickerControllerOriginalImage -> UIImage holding the original image
+ 
+ * @param attachmentBlock
+ *     This block is executed right after the end of processing image.
+ *     Parameters:
+ *      - AUAttachmentFile attachmentFile - object holding info about the original media and thumbnail image.
+
+ */
+
 - (NSUUID *)processImageWithPickerParams:(NSDictionary *)params
                    attachmentBlock:(void (^)(AUAttachmentFile *attachmentFile))block;
 
 /*!
- This method is responsible mainly for creating thumbnail image, based on the provided media, and storing both thumbnail and the original content in the path specified by bucketStoragePath.
  
- It takes as an input NSDictionary with similar structure to the UIImagePickerController's info dictionary.
- The basic key-values needed to be supplied are:
- - UIImagePickerControllerMediaType -> type of the media being processed (video)
- - UIImagePickerControllerMediaURL -> URL pointing to the video
+ * Creates and stores thumbnail based on the original video, exports video with default settings.
  
- thumbnailBlock is executed right after the end of creating thumbnail. The NSUUID variable holds the process unique id. The NSURL fileURL points to the original file, CGSize is the size of original image and NSURL thumbnailURL points to the created thumbnail.
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (video)
+ *      - UIImagePickerControllerMediaURL -> URL pointing to the video
  
- Then, after the end of processing video, the completionBlock is fired. The NSUUID variable holds the process unique id. The AVAssetExportSessionStatus holds the status of the encoding process, while NSError is the potential error, which may occur during the process. 
+ * @param thumbnailBlock
+ *     This block is executed right after the creation of thumbnail image.
+ *     Parameters:
+ *      - NSUUID process     - process unique id.
+ *      - NSURL fileURL      - points to the original file.
+ *      - CGSize size        - size of original image.
+ *      - NSURL thumbnailURL - points to the created thumbnail.
+ 
+ * @param completitionBlock
+ *     This block is executed right after the end of exporting movie.
+ *     Parameters:
+ *      - NSUUID process                    - process unique id.
+ *      - AVAssetExportSessionStatus status - status of the export session.
+ *      - NSError error                     - the potential error, which may occur during the process.
  
  */
+
 - (NSUUID *)processVideoWithPickerParams:(NSDictionary *)params
                       thumbnailBlock:(void (^)(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL))thumbnailBlock
                    completitionBlock:(void (^)(NSUUID *process, AVAssetExportSessionStatus status, NSError *error))processingBlock;
+
+/*!
+ 
+ * Creates and stores thumbnail based on the original video, exports video with custom settings.
+ 
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (video)
+ *      - UIImagePickerControllerMediaURL -> URL pointing to the video
+ 
+ * @param thumbnailBlock
+ *     This block is executed right after the creation of thumbnail image.
+ *     Parameters:
+ *      - NSUUID process     - process unique id.
+ *      - NSURL fileURL      - points to the original file.
+ *      - CGSize size        - size of original image.
+ *      - NSURL thumbnailURL - points to the created thumbnail.
+ 
+ * @param encoderSettingsBlock
+ *     This block is executed right before the begining of exporting movie.
+ *     Parameters:
+ *      - SDAVAssetExportSession encoder - fully customizable encoder used to export movie.
+ 
+ * @param completitionBlock
+ *     This block is executed right after the end of exporting movie.
+ *     Parameters:
+ *      - NSUUID process                    - process unique id.
+ *      - AVAssetExportSessionStatus status - status of the export session.
+ *      - NSError error                     - the potential error, which may occur during the process.
+ 
+ */
+
 
 - (NSUUID *)processVideoWithPickerParams:(NSDictionary *)params
                           thumbnailBlock:(void (^)(NSUUID *process, NSURL *fileURL, CGSize size, NSURL *thumbnailURL))thumbnailBlock
                     encoderSettingsBlock:(void (^)(SDAVAssetExportSession *encoder))settingsBlock
                        completitionBlock:(void (^)(NSUUID *process, AVAssetExportSessionStatus status, NSError *error))processingBlock;
 
+/*!
+ 
+ * Creates and stores thumbnail based on the original video, exports video with custom settings.
+ 
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (video)
+ *      - UIImagePickerControllerMediaURL -> URL pointing to the video
+ 
+ * @param attachmentBlock
+ *     This block is executed right after the creation of thumbnail image.
+ *     Parameters:
+ *      - AUAttachmentFile attachmentFile - object holding info about the original media and thumbnail image.
+ 
+ * @param encoderSettingsBlock
+ *     This block is executed right before the begining of exporting movie.
+ *     Parameters:
+ *      - SDAVAssetExportSession encoder - fully customizable encoder used to export movie.
+ 
+ * @param completitionBlock
+ *     This block is executed right after the end of exporting movie.
+ *     Parameters:
+ *      - NSUUID process                    - process unique id.
+ *      - AVAssetExportSessionStatus status - status of the export session.
+ *      - NSError error                     - the potential error, which may occur during the process.
+ 
+ */
+
+
 - (NSUUID *)processVideoWithPickerParams:(NSDictionary *)params
                          attachmentBlock:(void (^)(AUAttachmentFile *attachmentFile))block
                     encoderSettingsBlock:(void (^)(SDAVAssetExportSession *encoder))settingsBlock
                        completitionBlock:(void (^)(NSUUID *process, AVAssetExportSessionStatus status, NSError *error))processingBlock;
+
+/*!
+ 
+ * Creates and stores thumbnail based on the original video, exports video with default settings.
+ 
+ * @param params
+ *     NSDictionary with similar structure to the UIImagePickerController's info dictionary.
+ *     The basic key-values needed to be supplied are:
+ *      - UIImagePickerControllerMediaType -> type of the media being processed (video)
+ *      - UIImagePickerControllerMediaURL -> URL pointing to the video
+ 
+ * @param attachmentBlock
+ *     This block is executed right after the creation of thumbnail image.
+ *     Parameters:
+ *      - AUAttachmentFile attachmentFile - object holding info about the original media and thumbnail image.
+ 
+ * @param completitionBlock
+ *     This block is executed right after the end of exporting movie.
+ *     Parameters:
+ *      - NSUUID process                    - process unique id.
+ *      - AVAssetExportSessionStatus status - status of the export session.
+ *      - NSError error                     - the potential error, which may occur during the process.
+ 
+ */
+
 
 - (NSUUID *)processVideoWithPickerParams:(NSDictionary *)params
                          attachmentBlock:(void (^)(AUAttachmentFile *attachmentFile))block
